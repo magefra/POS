7cbd9979df927d8cd9d8da1975a6e629c9005348
magefra9 <magefra9@hotmail.com>
2023-01-05 12:17:39 -0600

    Agregando CategoryController

---
+++ POS.Api/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POS.Application.Dtos.Request;
+using POS.Application.Interfaces;
+using POS.Infraestructure.Commons.Bases.Request;
+
+namespace POS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryApplication _categoryApplication;
+
+        public CategoryController(ICategoryApplication categoryApplication)
+        {
+            _categoryApplication = categoryApplication;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ListCategories([FromBody] BaseFilterRequest filters)
+        {
+            var response = await _categoryApplication.ListCategories(filters);
+            return Ok(response);
+        }
+
+        [HttpGet("select")]
+        public async Task<IActionResult> ListSelectCategories()
+        {
+            var response = await _categoryApplication.ListSelectCategories();
+            return Ok(response);
+        }
+
+        [HttpGet("{categoryId:int}")]
+        public async Task<IActionResult> CategoryById(int categoryId)
+        {
+            var response = await _categoryApplication.CategoryById(categoryId);
+            return Ok(response);
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterCategory([FromBody] CategoryRequestDto categoryRequestDto)
+        {
+            var response = await _categoryApplication.RegisterCategory(categoryRequestDto);
+            return Ok(response);
+        }
+
+        [HttpPut("Edit/{categoryId:int}")]
+        public async Task<IActionResult> EditCategory(int categoryId, [FromBody] CategoryRequestDto categoryRequestDto)
+        {
+            var response = await _categoryApplication.EditCategory(categoryId, categoryRequestDto);
+            return Ok(response);
+        }
+
+        [HttpPut("Remove/{categoryId:int}")]
+        public async Task<IActionResult> RemoveCategory(int categoryId)
+        {
+            var response = await _categoryApplication.RemoveCategory(categoryId);
+            return Ok(response);
+        }
+
+
+    }
+}
