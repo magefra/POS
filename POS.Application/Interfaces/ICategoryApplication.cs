8b8fcef1ae40bb8357654436d6540fc6ae345368
magefra9 <magefra9@hotmail.com>
2023-01-05 10:47:58 -0600

    Agregando interfaz de ICategoryApplication

---
+++ POS.Application/Interfaces/ICategoryApplication.cs
@@ -0,0 +1,19 @@
+﻿using POS.Application.Base;
+using POS.Application.Dtos.Request;
+using POS.Application.Dtos.Response;
+using POS.Infraestructure.Commons.Bases.Request;
+using POS.Infraestructure.Commons.Bases.Response;
+
+namespace POS.Application.Interfaces
+{
+    public interface ICategoryApplication
+    {
+        Task<BaseResponse<BaseEntityResponse<CategoryResponseDto>>> ListCategories(BaseFilterRequest filter);
+
+        Task<BaseResponse<IEnumerable<CategorySelectResponseDto>>> ListSelectCategories();
+        Task<BaseResponse<CategoryResponseDto>> CategoryById(int categoryId);
+        Task<BaseResponse<bool>> RegisterCategory(CategoryRequestDto requestDto);
+        Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestDto requestDto);
+        Task<BaseResponse<bool>> RemoveCategory(int categoryId);
+    }
+}
---
+++ POS.Application/Services/CategoryApplication.cs
@@ -0,0 +1,42 @@
+﻿using POS.Application.Base;
+using POS.Application.Dtos.Request;
+using POS.Application.Dtos.Response;
+using POS.Application.Interfaces;
+using POS.Infraestructure.Commons.Bases.Request;
+using POS.Infraestructure.Commons.Bases.Response;
+
+namespace POS.Application.Services
+{
+    public class CategoryApplication : ICategoryApplication
+    {
+        public Task<BaseResponse<CategoryResponseDto>> CategoryById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> EditCategory(int categoryId, CategoryRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<BaseEntityResponse<CategoryResponseDto>>> ListCategories(BaseFilterRequest filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<IEnumerable<CategorySelectResponseDto>>> ListSelectCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> RegisterCategory(CategoryRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BaseResponse<bool>> RemoveCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
