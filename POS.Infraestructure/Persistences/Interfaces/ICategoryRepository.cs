cd58109b9bfa58e379c2c085af06adbc405fcf9a
magefra9 <magefra9@hotmail.com>
2023-01-06 12:13:07 -0600

    Implementando IGenericRepository en ICategoryRepository

--- POS.Infraestructure/Persistences/Interfaces/ICategoryRepository.cs
+++ POS.Infraestructure/Persistences/Interfaces/ICategoryRepository.cs
@@ -4,7 +4,7 @@ using POS.Infraestructure.Commons.Bases.Response;
 
 namespace POS.Infraestructure.Persistences.Interfaces
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IGenericRepository<Category>
     {
         Task<BaseEntityResponse<Category>> ListCategories(BaseFilterRequest filters);
     }
