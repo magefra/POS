4c0412f1b9c6f4ec2f948bbdcb9790c8df97e848
magefra9 <magefra9@hotmail.com>
2023-01-13 18:02:28 -0600

    Agregando response para user.

---
+++ POS.Application/Dtos/User/Request/UserRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Application.Dtos.User.Request
+{
+    public class UserRequestDto
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ POS.Application/Services/UserApplication.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using POS.Application.Base;
+using POS.Application.Dtos.User.Request;
+using POS.Application.Interfaces;
+using POS.Domain.Entities;
+using POS.Infraestructure.Persistences.Interfaces;
+using POS.Utilities.Static;
+using System.Text;
+using BC = BCrypt.Net.BCrypt;
+
+namespace POS.Application.Services
+{
+    public class UserApplication : IUserApplication
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        public UserApplication(IUnitOfWork unitOfWork, IMapper mapper, IConfiguration configuration)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public async Task<BaseResponse<bool>> RegisterUser(UserRequestDto requestDto)
+        {
+            var response = new BaseResponse<bool>();
+            var account = _mapper.Map<User>(requestDto);
+
+            account.Password = BC.HashPassword(account.Password);
+            response.Data = await _unitOfWork.User.RegisterAsync(account);
+
+            if (response.Data)
+            {
+                response.IsSuccesss = true;
+                response.Message = ReplyMessage.MESSAGE_SAVE;
+            }
+            else
+            {
+                response.IsSuccesss = false;
+                response.Message = ReplyMessage.MESSAAGE_FAILDER;
+            }
+
+            return response;
+        }
+
+        public async Task<BaseResponse<string>> GenerateToken(TokenRequestDto tokenDto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration[""]));
+        }
+    }
+}
