2aaa2d3bac68558dc4eb8579b0ed3f81f97de7e6
magefra9 <magefra9@hotmail.com>
2023-01-17 18:36:14 -0600

    Agregando SwaggerExtensions

---
+++ POS.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace POS.Api.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            var openApi = new OpenApiInfo
+            {
+                Title = "POS Api",
+                Version = "v1",
+                Description = "Punto de venta API 2023",
+                TermsOfService = new Uri("https://github.com/magefra"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Magdiel Palacios",
+                    Email = "magefra9@hotmail.com",
+                    Url = new Uri("https://github.com/magefra"),
+                }
+
+            };
+
+            services.AddSwaggerGen(x =>
+            {
+                openApi.Version = "v1";
+                x.SwaggerDoc("v1", openApi);
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "JWT Bearer Token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                x.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[]{ } }
+                });
+            });
+            return services;
+        }
+    }
+}
