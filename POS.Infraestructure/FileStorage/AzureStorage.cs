3e446dc7bb38c2dbaddd34e591f1da992bda8383
magefra9 <magefra9@hotmail.com>
2023-01-18 10:18:16 -0600

    Agregando IAzureStorage

---
+++ POS.Infraestructure/FileStorage/AzureStorage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace POS.Infraestructure.FileStorage
+{
+    public class AzureStorage : IAzureStorage
+    {
+
+
+        public Task<string> EditFile(string container, IFormFile file, string route)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RemoveFile(string route, string container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveFile(string container, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
