cc6a3d21ef9b4f33b3a727618fc029f019f856c6
magefra9 <magefra9@hotmail.com>
2023-01-25 17:37:26 -0600

    Implementando ProviderRepository

--- POS.Infraestructure/Persistences/Repositories/ProviderRepository.cs
+++ POS.Infraestructure/Persistences/Repositories/ProviderRepository.cs
@@ -1,4 +1,5 @@
-﻿using POS.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
 using POS.Infraestructure.Commons.Bases.Request;
 using POS.Infraestructure.Commons.Bases.Response;
 using POS.Infraestructure.Persistences.Interfaces;
@@ -14,7 +15,47 @@ namespace POS.Infraestructure.Persistences.Repositories
 
         public async Task<BaseEntityResponse<Provider>> ListProviders(BaseFilterRequest filters)
         {
-            throw new NotImplementedException();
+            var response = new BaseEntityResponse<Provider>();
+
+            var providers = GetEntityQuery(x => x.AuditDeleteUser == null && x.AuditCreateDate == null)
+                .AsNoTracking();
+
+            if (filters.NumFilter is not null && !string.IsNullOrEmpty(filters.TextFilter))
+            {
+                switch (filters.NumFilter)
+                {
+                    case 1:
+                        providers = providers.Where(x => x.Name!.Contains(filters.TextFilter));
+                        break;
+                    case 2:
+                        providers = providers.Where(x => x.Email!.Contains(filters.TextFilter));
+                        break;
+                    case 3:
+                        providers = providers.Where(x => x.DocumentNumber!.Contains(filters.TextFilter));
+                        break;
+                }
+
+            }
+            if (filters.StateFilter is not null)
+            {
+                providers = providers.Where(x => x.State.Equals(filters.StateFilter));
+            }
+
+            if (filters.StartDate is not null && filters.EndDate is not null)
+            {
+                // Filtros por fecha.
+                providers = providers.Where(
+                    x => x.AuditCreateDate >= Convert.ToDateTime(filters.StartDate)
+                    && x.AuditCreateDate <= Convert.ToDateTime(filters.EndDate)
+                    .AddDays(1));
+            }
+
+            if (filters.Sort is null) filters.Sort = "Id";
+
+            response.TotalRecords = await providers.CountAsync();
+            response.Items = await Ordering(filters, providers, !(bool)filters.Download!).ToListAsync();
+
+            return response;
         }
     }
 }
